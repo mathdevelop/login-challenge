07ec6b59449f48dfc9c00c86c62840c2a4c5019e
Matheus Alves <web.matheusalves@gmail.com>
2021-06-25 09:25:30 -0300

    Updating fetch policy (no-cache)

--- client/src/services/apollo.js
+++ client/src/services/apollo.js
@@ -1,8 +1,18 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
+const defaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'no-cache'
+  },
+  query: {
+    fetchPolicy: 'no-cache'
+  }
+};
+
 const client = new ApolloClient({
   uri: process.env.REACT_APP_SERVER,
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  defaultOptions
 });
 
 export default client;
